a5b594320c19ab51dfd4ddd5105ed1aab0399361
Carlos Jessé Coelho <tv.coelho01@gmail.com>
2023-06-29 15:04:06 -0300

    ex002 e ex003

---
+++ cursoEmVideo/ex002.js
@@ -0,0 +1,8 @@
+var nome = window.prompt('qual seu nome zé?')
+window.alert('Ja te conhecia ne ' + nome + '!')
+/* ex003 */
+
+var n1 = parseInt(window.prompt('Fala um numero ai?'))
+var n2 = Number(window.prompt('fala outro numero?'))
+var s =  n1 +n2
+document.write('A soma da: ', s)
