56446304ecaf8b55b121a345e98d0c5e77283992
Carlos Jessé Coelho <63757384+RECCOELHO@users.noreply.github.com>
2025-03-11 16:11:54 -0300

    TERMINEI O CONVERSOR DE BASES

--- cursoEmVideo/CursodePython/conversordebases.py
+++ cursoEmVideo/CursodePython/conversordebases.py
@@ -2,16 +2,14 @@ num = int(input("Digite um numero:"))
 print('''Escolha ums das bases para conversão:
       [ 01 ] Para converter em BINARIO
       [ 02 ] Para converter em OCTAL
-      [ 03 ] Para converter  em HEXADECIMAL''')
+      [ 03 ] Para converter em HEXADECIMAL''')
 OPCAO = int(input('Qual opção?'))
-
 if OPCAO == 1:
-    binario = num / 2
-    print('{}convertido para BINARIO é igual a {}')
+    print('{} convertido para BINARIO é igual a {}'.format(num, bin(num)[2:]))
 
-if OPCAO == 2:
-    print('{}convertido para octal é igual a {}')
+elif OPCAO == 2:
+    print('{} convertido para octal é igual a {}'.format(num, oct(num)[2:]))
 
-if OPCAO == 3:
-    print('{} convertido para HEXADECIMAL é igual a {}')
-else: print('Opção invalida'))
+elif OPCAO == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+else: print('###Opção invalida.Tente novamente.###')
